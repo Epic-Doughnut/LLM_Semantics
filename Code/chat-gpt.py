ed154172ee9b43eaaf45b62e853c50a9b251d480
Joey Sherrill <31806536+Epic-Doughnut@users.noreply.github.com>
2024-10-25 15:23:32 -0600

    attempt to call gpt

--- Code/chat-gpt.py
+++ Code/chat-gpt.py
@@ -1,27 +1,28 @@
-import os
-import openai
+# import os
+# import openai
 from dotenv import load_dotenv
+
+# # Load environment variables from .env file
 load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# # Load OpenAI API key from environment variable
+# openai.api_key = os.getenv("OPENAI_API_KEY")
 
-chat_completion = openai.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Say this is a test",
-        }
-    ],
-    model="gpt-3.5-turbo",
+# Define the GPT-4 model for chat completion.
+# GPT_MODEL = "gpt-4o-mini"
+from openai import OpenAI 
+import os
+
+## Set the API key and model name
+MODEL="gpt-4o-mini"
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+completion = client.chat.completions.create(
+  model=MODEL,
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant. Help me with my math homework!"}, # <-- This is the system message that provides context to the model
+    {"role": "user", "content": "Hello! Could you solve 2+2?"}  # <-- This is the user message for which the model will generate a response
+  ]
 )
 
-# def get_completion(prompt, model="gpt-3.5-turbo"):
-# 	messages = [{"role": "user", "content": prompt}]
-
-# 	response = openai.ChatCompletion.create(	model=model,	messages=messages,	temperature=0,	)
-# 	return response.choices[0].message["content"]
-
-if __name__ == "__main__":
-    
-    print(chat_completion)
-    
+print("Assistant: " + completion.choices[0].message.content)
