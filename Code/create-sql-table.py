40bd8ac3385b1cd3904049c93064b7e709a6b9ca
Joey Sherrill <31806536+Epic-Doughnut@users.noreply.github.com>
2025-03-26 13:10:07 -0600

    Add book recommendation dataset and data cleaning functionality

---
+++ Code/create-sql-table.py
@@ -0,0 +1,51 @@
+import mysql.connector
+import pandas as pd
+
+def establish_db():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="admin",
+        password="password"
+    )
+    
+    return mydb
+
+def clean_csv(path:str):
+    
+    # books = pd.read_csv(path + "Books.csv")
+    ratings = pd.read_csv(path + "Ratings.csv")
+    users = pd.read_csv(path + "Users.csv")
+    
+    # books = books.drop(columns=['Image-URL-S', 'Image-URL-M', 'Image-URL-L'])
+    
+    # remove all the ratings of 0
+    ratings = ratings[ratings['Book-Rating'] != 0]
+    
+    # remove all the users without age data
+    users = users[pd.notna(users['Age'])]
+    
+    # remove all the users outside our age range (12-19)
+    users = users[users['Age'] >= 12]
+    users = users[users['Age'] <= 19]
+    
+    # remove all the users who have not rated any books
+    users = users[users['User-ID'].isin(ratings['User-ID'])]
+    
+    # keep only the ratings of users which are in the users dataframe
+    ratings = ratings[ratings['User-ID'].isin(users['User-ID'])]
+    
+    
+    # write the cleaned data to a new file
+    ratings.to_csv(path + "Ratings_cleaned.csv", index=False)
+    users.to_csv(path + "Users_cleaned.csv", index=False)
+    
+    print("Done cleaning csv files")
+    print(f"Total of {len(ratings)} ratings")
+    print(f"Total of {len(users)} users")
+        
+
+
+if __name__ == '__main__':
+    clean_csv("../data/archive/")
+    # mydb = establish_db()
+    # print(mydb)
---
+++ Code/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+def establish_db():
+#     mydb = mysql.connector.connect(
+#         host="localhost",
+#         user="admin",
+#         password="password"
+#     )
+    
+#     return mydb
+
+# if __name__ == '__main__':
+#     mydb = establish_db()
+#     print(mydb)
